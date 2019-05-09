3fc945bfeb41d51a71c56fbea79ff4c66b41a1eb
= <=>
2019-05-10 05:10:37 +0900

    add calculation function

--- R/food_composition.R
+++ R/food_composition.R
@@ -1,7 +1,6 @@
 extractValueFromVector = function(target, keyword, ignore_case = T) {
     index <- grep(x = target, pattern = keyword, ignore.case = ignore_case)
-    result <- target[index]
-    return(result)
+    return(target[index])
 }
 
 extractSubsetFromFoodGroup = function(target_food_group) {
@@ -26,12 +25,19 @@ getFoodGroups = function() {
     return(unique(food_composition$food_group))
 }
 
+menufindFood <- function() {
+    group <- getFoodGroups()[menu(getFoodGroups(), graphics = TRUE)]
+    keyword = readline("please input keyword...")
+    res1 <- findFood(keyword = keyword, food_group = group)
+    return(res1[menu(res1[, "food_and_description"], graphics = TRUE), ])
+}
+
 findFood = function(keyword, food_group = NULL) {
     data("food_composition")
     if (is.character(food_group)) {
         food_composition <- extractSubsetFromFoodGroup(food_group)
     }
-    subset_food <- subset(food_composition, grepl(keyword, food_composition$food_and_description,ignore.case = T))
+    subset_food <- subset(food_composition, grepl(keyword, food_composition$food_and_description, ignore.case = T))
     if (dim(subset_food)[1] == 0) {
         stop("not found input keyword.")
     }
@@ -50,7 +56,34 @@ subsetFoodRichIn = function(nutrient_name, food_group = NULL, n = 10) {
 
     food_composition <- food_composition[order(food_composition[index], decreasing = T), ]
     if (dim(food_composition)[1] == 0) {
-        stop("not found enter food group.")
+        stop("not found input food group.")
     }
     return(head(food_composition, n))
+}
+
+getFoodNamesByFoodNumbers = function(food_numbe_list) {
+    data("food_composition")
+    return(food_composition[food_numbe_list, "food_and_description"])
+}
+
+calcNutrientMultipledays = function(food_list_multiple) {
+    if (is.data.frame(food_list_multiple)) {
+        stop("If you want to calculate a meal for one day, use the calcNutrient().")
+    }
+    ret <- sapply(food_list_multiple, calcNutrient)
+    mean_nutrient <- round(rowMeans(ret), 3)
+    return(mean_nutrient)
+}
+
+calcNutrient = function(food_list = food1) {
+    data("food_composition")
+    food_number <- food_list$food_number
+    food_weight <- food_list$food_weight
+    # this nutrient_data is nutrient value only.
+    # in addition, 6 is index that nutrient start (energy).
+    # last nutrient item (Yield) is not included.
+    nutrient_data <- food_composition[food_number, 6:ncol(food_composition) - 1]
+    nutrient_result <- nutrient_data * 0.01 * food_list$weight
+    nutrient_total <- colSums(nutrient_result)
+    return(nutrient_total)
 }
